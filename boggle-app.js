29f960f3fabcdab6956ab4810666653aac184144
Quincey Hollman <qhollman@gmail.com>
2020-01-29 16:28:56 -0800

    edited rollDice function

--- boggle-app.js
+++ boggle-app.js
@@ -8,20 +8,45 @@ const diceList = [
     'exavnd', 'ralesc', 'uwilrg', 'pacemd'
     ];
 //create function to roll dice 
+let rolledDie = []; //declarerolled die as global variable to keep from submitting duplicate dice onto board;
 
 function rollDice(array) {
 //select random die
+
 let randomDie = array[Math.floor(Math.random() * 16)];
 console.log(randomDie);
+
+//need to find a way to remove array items that have already been output by this function
+let rolledDieList = rolledDie.concat(randomDie);
+if (!rolledDieList.includes(randomDie)) {
+    rollDice(array)
+} 
+ console.log(rolledDie);
+// random die has already been remove from array
 
 //randomly select letter on die to be displayed
 let randomLetter = randomDie[Math.floor(Math.random() * 6)];
 console.log(randomLetter)
 // if Q add u
-
+if (randomLetter === 'Q') {
+    randomLetter = 'Qu'
+}
+  return rolledDieList
 }
 console.log(rollDice(diceList));
+
+console.log(rollDice(diceList));
+
+console.log(rollDice(diceList));
+
+console.log(rollDice(diceList));
+
+
+
 //create a function to output the location of the button
 // and match with each random die with its displaying letter
 
 //create board locations with matching buttons from the 
+
+//use rollDice function to input display on buttons on game board. 
+
